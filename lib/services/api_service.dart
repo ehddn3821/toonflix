7c0a9f0a0780fd08c261093edbe69224029e6700
ehddn3821 <ehddn3821@naver.com>
2024-04-05 15:58:36 +0900

    feat: Add api service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return;
+    }
+    throw Error();
+  }
+}
